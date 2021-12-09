5e7d6ef4a88678e3d856cecd777cf450e68819de
Josué Ramírez <josueramirez0110@gmail.com>
2021-12-08 20:11:03 -0600

    Added database configuration

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ const util = require('util');
 
 const pool = mysql.createPool({
     connectionLimit: 5,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'randm'     
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'bba56a664ac653',
+    password: '33a902c8',
+    database: 'heroku_cbbb49b72c097f0'     
 });
 
 pool.query = util.promisify(pool.query);
